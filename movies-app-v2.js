efe0372a610a89e212af32448282f0b2514deee1
SerguioL <serguio.l.landa@gmail.com>
2021-09-06 15:38:00 -0500

    got edit button to work

--- movies-app-v2.js
+++ movies-app-v2.js
@@ -3,31 +3,61 @@ const GLITCH = 'https://uneven-organic-meteoroid.glitch.me/movies';
 const getMovies = () => fetch(GLITCH)
     .then(res => res.json())
     .then(data => {
+        $("#cards").html("")
         console.log(data)
         data.forEach(function (movie) {
-            $('#cards').append('<article>' +
-                '<p>' + movie.title + '</p>' +
-                '<p>' + movie.rating + '</p>' +
-                `<button class="edit" data-id="${movie.id}" >Edit</button>'` +
-                `<button class="delete" data-id="${movie.id}" >Delete</button>` +
-                '</article>');
+            $('#cards').append(`<article>
+<p>${movie.title}</p>
+<p>${movie.rating}</p>
+<button class="edit" >Edit</button>
+<button class="delete" data-id="${movie.id}" >Delete</button>
+<ul class="hidden">
+    <li>
+        <input class="addRating" name="rating" type="text" placeholder="Title"/>
+    </li>
+    <li>
+        <input class="addTitle" name="title" type="text" placeholder="Rating"/>
+    </li>
+    <li>
+        <button type="submit" data-id="${movie.id}" class="submitM">Submit</button>
+    </li>
+</ul>
+</article>`);
         });
         $('.delete').click(function () {
             var id = $(this).attr("data-id");
             console.log(id);
-            $("article").remove();
+            // $("article").remove();
             deleteMovie(id);
 
         });
 
         $('.edit').click(function () {
-            var id = $(this).attr("data-id");
-            console.log(id);
+            $("ul").toggleClass("hidden")
+            //
+            // var id = $(this).attr("data-id");
+            // console.log(id);
 
             // $("article").remove();
             // editMovie(id);
 
         });
+
+        $(".submitM").click(function (){
+            var newMovieT = $(this)[0].parentElement.parentElement.children[0].children[0].value
+            var newMovieR = $(this)[0].parentElement.parentElement.children[1].children[0].value
+            var newMovie = {
+                title: newMovieT,
+                rating: newMovieR,
+                id: $(this).attr("data-id")
+            }
+            console.log(newMovie);
+            // $("article").remove();
+            editMovie(newMovie);
+
+        });
+
+
 
 
     })
